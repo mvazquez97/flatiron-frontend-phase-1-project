f5dff3ae7aadba37af4b93b0c5e7e17914b81924
Manuel <manuelvazquez76@yahoo.com>
2023-07-20 06:27:50 -0700

    gets city name from input

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-    getData();
-  });
 
- 
-// function search() {
-//     document.getElementsByClassName("search-button").addEventListener("click", getData());
-// }
+
+let cityName=null;
+
+
+//getting city name from input
+let inputElement=document.querySelector('.weather-search-form');
+inputElement.addEventListener('change',handleTextChange);
+function handleTextChange(event){
+cityName=event.target.value
+}
+
+
 
  let searchBtn = document.querySelector(".search-button")
- searchBtn.addEventListener("click", () => getData());
+ searchBtn.addEventListener("click", getData);
 
 
 function getData() {
-    fetch("http://localhost:3000/cities")
+  if(!cityName){
+    return alert('please enter city name')
+  }
+    fetch("/db.json")
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      let cities=json.cities;
+      let city=cities.filter(obj=>obj.name.toLowerCase()==cityName.toLowerCase())[0];
+      if(!city){
+       return alert('city not found in data')
+      }
+
+      document.querySelector(".weather-city").innerHTML=city.name.toString();
+      document.querySelector(".weather-forecast p").innerHTML=city.currentWeather.toString();
+      document.querySelector(".weather-temperature").innerHTML=city.temperature.toString();
+    })
 }
 
